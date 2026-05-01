4d992614a0dace709cf021a737b3dc17c60b191f
Lucio <Lucio.Vega@alu.frt.utn.edu.ar>
2026-05-01 18:31:30 -0300

    Resolucion del problema 2

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -8,7 +8,14 @@
             /*ProductHelper helper = new ProductHelper();
 
             string etiqueta = helper.ObtenerEtiquetaProducto(1001, "Coca Cola 500ml", 1500.50m);
-            Console.WriteLine(etiqueta);*/
+            Console.WriteLine(etiqueta);
+            ==========================================
+            ==========================================*/
+            //EJERCICIO 2 COMPILACION
+           /* Problema2 p2 = new Problema2();
+            Console.WriteLine(p2.CrearResumenVenta(1001, "Fernet Branca 750ml", 3, 9500.50m));
+            Console.WriteLine(p2.CrearResumenVenta(1001, "Fernet Branca 750ml", -1, 9500.50m)); // quantity <= 0, total = 0
+           En el segundo me fijo si realmente el total da 0 al ser la cantidad <=0 */
         }
     }
 }
